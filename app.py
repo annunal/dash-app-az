95e784cc12ad8afd3704164458276a82c2bc67e6
annunal <alessandro.annunziato@gmail.com>
2024-04-12 07:07:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ import pandas as pd
 
 df = pd.read_csv('assets/data/gapminderDataFiveYear.csv')
 
-dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app = dash_app.server
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
 
-dash_app.layout = html.Div([
+app.layout = html.Div([
     dcc.Graph(id='graph-with-slider'),
     dcc.Slider(
         id='year-slider',
@@ -30,7 +30,7 @@ dash_app.layout = html.Div([
 #dash_app.layout=html.Div('Alessandro')
 
 
-@dash_app.callback(
+@app.callback(
     Output('graph-with-slider', 'figure'),
     Input('year-slider', 'value'))
 
@@ -47,4 +47,4 @@ def update_figure(selected_year):
 
 
 if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+    app.run_server(debug=True)
